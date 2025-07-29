89ae73b255b62d32db04548914310108b3454e53
Yashaswini2004 <yashaswinic31@gmail.com>
2025-07-29 11:52:19 +0530

    recursion

---
+++ recursion/permutations.java
@@ -0,0 +1,18 @@
+public class permutations{
+    public static void permutation(String str,String perm){
+         if(str.length()==0){
+            System.out.println(perm);
+            return;
+         }
+        for(int i=0;i<str.length();i++)
+        {
+            char ch=str.charAt(i);
+            String newStr=str.substring(0,i) + str.substring(i+1);
+            permutation(newStr,perm+ch);
+        }
+    }
+    public static void main(String[] args){
+        String str="abc";
+        permutation(str,"");
+    }
+}
